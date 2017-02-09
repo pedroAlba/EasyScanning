5995da65a66dc478876e9003ddf9b2d70465a2af
Pedro H. P Alba <pedrohalba@hotmail.com>
2017-02-09 00:23:16 -0200

    First commit
    Added this local repo to github.

---
+++ EasyScanning/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace EasyScanning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNomeProduto = new System.Windows.Forms.TextBox();
+            this.btScan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Items.AddRange(new object[] {
+            "Eletrodoméstico",
+            "Eletrônico",
+            "Itens variados"});
+            this.cbCategoria.Location = new System.Drawing.Point(84, 44);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbCategoria.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selecione o tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Informe o nome do produto";
+            // 
+            // tbNomeProduto
+            // 
+            this.tbNomeProduto.Location = new System.Drawing.Point(67, 124);
+            this.tbNomeProduto.Name = "tbNomeProduto";
+            this.tbNomeProduto.Size = new System.Drawing.Size(154, 20);
+            this.tbNomeProduto.TabIndex = 3;
+            // 
+            // btScan
+            // 
+            this.btScan.Location = new System.Drawing.Point(16, 164);
+            this.btScan.Name = "btScan";
+            this.btScan.Size = new System.Drawing.Size(256, 85);
+            this.btScan.TabIndex = 4;
+            this.btScan.Text = "Escanear";
+            this.btScan.UseVisualStyleBackColor = true;
+            this.btScan.Click += new System.EventHandler(this.btScan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btScan);
+            this.Controls.Add(this.tbNomeProduto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCategoria);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNomeProduto;
+        private System.Windows.Forms.Button btScan;
+    }
+}
+
---
+++ EasyScanning/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace EasyScanning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cbCategoria.SelectedIndex = 0;
+            this.CenterToScreen();
+        }
+
+        private void btScan_Click(object sender, EventArgs e)
+        {
+            var tipo = cbCategoria.SelectedItem.ToString();
+
+            var nome = tbNomeProduto.Text;
+
+            WinScanner.Scan(tipo, nome);
+
+            tbNomeProduto.Text = "";
+
+            this.BringToFront();
+        }
+    }
+       
+}
